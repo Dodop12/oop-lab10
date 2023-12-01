73519f184b4dd699b0fa1ca3ede94927453dbdb4
Edoardo Pierfederici <dodop2002pierfe@gmail.com>
2023-12-01 09:03:07 +0100

    fix: null file management

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/ConfigFromFile.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/ConfigFromFile.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.StringTokenizer;
 
 /**
@@ -29,44 +30,48 @@ public final class ConfigFromFile {
     public ConfigFromFile(final DrawNumberView... views) {
         confBuilder = new Configuration.Builder();
         try {
-            final Path filePath = Path.of(ClassLoader.getSystemResource(FILE_NAME).toURI());
-            final List<String> lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+            final var fileURL = ClassLoader.getSystemResource(FILE_NAME);
+            int lineNumber = 1; // Used by the error log
 
-            for (final String line : lines) {
-                // Splits the lines into tokens (words separated by a colon and a space by
-                // default)
-                final var tokenizer = new StringTokenizer(line, ": ");
-                int lineNumber = 1; // Used by the error log
+            if (Objects.isNull(fileURL)) {
+                DrawNumberApp.displayErrorAll("Cannot find the file '" + FILE_NAME + "'. " + DEFAULTVALUES_ERROR,
+                        views);
+            } else {
+                final Path filePath = Path.of(fileURL.toURI());
+                final List<String> lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
 
-                // Each line of the file must have the format 'attribute: value' (colon and
-                // space between the two are needed)
-                if (tokenizer.countTokens() == 2) {
-                    // First word of the line; ignoring case
-                    final String attribute = tokenizer.nextToken().toLowerCase(Locale.ROOT);
-                    // Second word of the line (must be an integer)
-                    final int value = Integer.parseInt(tokenizer.nextToken());
+                for (final String line : lines) {
+                    // Splits the lines into tokens (words separated by a colon and a space by
+                    // default)
+                    final var tokenizer = new StringTokenizer(line, ": ");
 
-                    switch (attribute) {
-                        case MIN -> confBuilder.setMin(value);
-                        case MAX -> confBuilder.setMax(value);
-                        case ATTEMPTS -> confBuilder.setAttempts(value);
-                        default -> DrawNumberApp.displayErrorAll(
-                                "Configuration file format error: invalid attribute (line " + lineNumber + ")", views);
-                    }
-                } else {
-                    DrawNumberApp
-                            .displayErrorAll(
-                                    "Configuration file format error: each line must contain exactly two words (line "
-                                            + lineNumber + ")",
+                    // Each line of the file must have the format 'attribute: value' (colon and
+                    // space between the two are needed)
+                    if (tokenizer.countTokens() == 2) {
+                        // First word of the line; ignoring case
+                        final String attribute = tokenizer.nextToken().toLowerCase(Locale.ROOT);
+                        // Second word of the line (must be an integer)
+                        final int value = Integer.parseInt(tokenizer.nextToken());
+
+                        switch (attribute) {
+                            case MIN -> confBuilder.setMin(value);
+                            case MAX -> confBuilder.setMax(value);
+                            case ATTEMPTS -> confBuilder.setAttempts(value);
+                            default -> DrawNumberApp.displayErrorAll(
+                                    "Configuration file format error: invalid attribute (line " + lineNumber + ")",
                                     views);
-                }
+                        }
+                    } else {
+                        DrawNumberApp
+                                .displayErrorAll(
+                                        "Configuration file format error: each line must contain exactly two words (line "
+                                                + lineNumber + ")",
+                                        views);
+                    }
 
-                lineNumber++;
+                    lineNumber++;
+                }
             }
-        } catch (final NullPointerException e) {
-            e.printStackTrace();
-            DrawNumberApp.displayErrorAll("Cannot find the file '" + FILE_NAME + "'. " + DEFAULTVALUES_ERROR,
-                    views);
         } catch (final IOException e) {
             e.printStackTrace();
             DrawNumberApp.displayErrorAll("Cannot read the file '" + FILE_NAME + "'. " + DEFAULTVALUES_ERROR,
